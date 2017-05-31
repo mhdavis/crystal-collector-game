3fdbf5cf7573b03b2b9d5f5f384f54e4fe9bb1bd
Michael Davis <mhdavis1993@gmail.com>
2017-05-31 16:57:53 -0400

    fixed js logic and expanded out basic html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,14 @@ guessValue
 totalsWins
 totalLoses
 
-
 */
+
+let winNum = 0;
+$("wins").html(winNum);
+let loseNum = 0;
+$("losses").html(loseNum);
+
+startGame();
 
 function startGame () {
   let userSum = 0;
@@ -21,8 +27,51 @@ function startGame () {
   let randomNumArray = [randomNumOne, randomNumTwo, randomNumThree, randomNumFour];
 }
   $("jewel-one").on("click", function () {
-    
+    userSum += randomNumOne;
   });
+
+  $("jewel-two").on("click", function () {
+    userSum += randomNumTwo;
+  });
+
+  $("jewel-three").on("click", function () {
+    userSum += randomNumThree;
+  });
+
+  $("jewel-four").on("click", function () {
+    userSum += randomNumFour;
+  });
+
+  if (userSum === guessValue) {
+
+    winNum++;
+    $("jewel-one").off('click');
+    $("jewel-two").off('click');
+    $("jewel-three").off('click');
+    $("jewel-four").off('click');
+
+    randomNumOne = randomIntFromInterval(1, 10);
+    randomNumTwo = randomIntFromInterval(10, 20);
+    randomNumThree = randomIntFromInterval(20, 30);
+    randomNumFour = randomIntoFromInterval(30, 50);
+
+    return $("#wins").html(winNum);
+
+  } else if (userSum > guessValue) {
+
+    loseNum++;
+    $("jewel-one").off('click');
+    $("jewel-two").off('click');
+    $("jewel-three").off('click');
+    $("jewel-four").off('click');
+
+    randomNumOne = randomIntFromInterval(1, 10);
+    randomNumTwo = randomIntFromInterval(10, 20);
+    randomNumThree = randomIntFromInterval(20, 30);
+    randomNumFour = randomIntoFromInterval(30, 50);
+
+    return $("#losses").html(loseNum);
+  }
 
 }); // end document.ready
 
