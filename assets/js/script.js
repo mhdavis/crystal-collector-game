301ce92aec80abcf8c3e926314f1e125365162b0
Michael Davis <mhdavis1993@gmail.com>
2017-06-01 13:45:12 -0400

    Add minor css for visiblility and fixed js logic; introduced bug where game only runs once, but now accesses win/lose conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,9 @@ totalLoses
 */
 
 let winNum = 0;
-$("wins").html(winNum);
+$("#wins").html(winNum);
 let loseNum = 0;
-$("losses").html(loseNum);
+$("#losses").html(loseNum);
 
 startGame();
 
@@ -23,55 +23,70 @@ function startGame () {
   let randomNumOne = randomIntFromInterval(1, 10);
   let randomNumTwo = randomIntFromInterval(10, 20);
   let randomNumThree = randomIntFromInterval(20, 30);
-  let randomNumFour = randomIntoFromInterval(30, 50);
-  let randomNumArray = [randomNumOne, randomNumTwo, randomNumThree, randomNumFour];
-}
-  $("jewel-one").on("click", function () {
-    userSum += randomNumOne;
-  });
+  let randomNumFour = randomIntFromInterval(30, 50);
 
-  $("jewel-two").on("click", function () {
-    userSum += randomNumTwo;
-  });
+  function reset () {
+    if (userSum === guessValue) {
+      console.log("entered win condition");
+      winNum++;
+      $("#jewel-one").off('click');
+      $("#jewel-two").off('click');
+      $("#jewel-three").off('click');
+      $("#jewel-four").off('click');
 
-  $("jewel-three").on("click", function () {
-    userSum += randomNumThree;
-  });
+      randomNumOne = randomIntFromInterval(1, 10);
+      randomNumTwo = randomIntFromInterval(10, 20);
+      randomNumThree = randomIntFromInterval(20, 30);
+      randomNumFour = randomIntFromInterval(30, 50);
+      $("#wins").html(winNum);
 
-  $("jewel-four").on("click", function () {
-    userSum += randomNumFour;
-  });
 
-  if (userSum === guessValue) {
+      return $("#wins").html(winNum);
 
-    winNum++;
-    $("jewel-one").off('click');
-    $("jewel-two").off('click');
-    $("jewel-three").off('click');
-    $("jewel-four").off('click');
+    } else if (userSum > guessValue) {
+      console.log("entered lose condition");
+      loseNum++;
+      $("#jewel-one").off('click');
+      $("#jewel-two").off('click');
+      $("#jewel-three").off('click');
+      $("#jewel-four").off('click');
 
-    randomNumOne = randomIntFromInterval(1, 10);
-    randomNumTwo = randomIntFromInterval(10, 20);
-    randomNumThree = randomIntFromInterval(20, 30);
-    randomNumFour = randomIntoFromInterval(30, 50);
+      randomNumOne = randomIntFromInterval(1, 10);
+      randomNumTwo = randomIntFromInterval(10, 20);
+      randomNumThree = randomIntFromInterval(20, 30);
+      randomNumFour = randomIntFromInterval(30, 50);
 
-    return $("#wins").html(winNum);
-
-  } else if (userSum > guessValue) {
-
-    loseNum++;
-    $("jewel-one").off('click');
-    $("jewel-two").off('click');
-    $("jewel-three").off('click');
-    $("jewel-four").off('click');
-
-    randomNumOne = randomIntFromInterval(1, 10);
-    randomNumTwo = randomIntFromInterval(10, 20);
-    randomNumThree = randomIntFromInterval(20, 30);
-    randomNumFour = randomIntoFromInterval(30, 50);
-
-    return $("#losses").html(loseNum);
+      return $("#losses").html(loseNum);
+    }
   }
+
+  $("#display-value").html(guessValue);
+  $("#player-value").html(userSum);
+
+  $("#jewel-one").on("click", function () {
+    userSum += randomNumOne;
+    $("#player-value").html(userSum);
+    reset();
+  });
+
+  $("#jewel-two").on("click", function () {
+    userSum += randomNumTwo;
+    $("#player-value").html(userSum);
+    reset();
+  });
+
+  $("#jewel-three").on("click", function () {
+    userSum += randomNumThree;
+    $("#player-value").html(userSum);
+    reset();
+  });
+
+  $("#jewel-four").on("click", function () {
+    userSum += randomNumFour;
+    $("#player-value").html(userSum);
+    reset();
+  });
+}
 
 }); // end document.ready
 
